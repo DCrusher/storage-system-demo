28962523fc15cf06d8abf30fb9d0875a78388ada
DCrusher <d.amuscias@gmail.com>
2019-09-20 02:44:55 +0500

    Updates

---
+++ precache-manifest.a4664267eb3280085ce8d01c07a4a18b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9b3ebef8249e58a6fc5926cf97b410cb",
+    "url": "/storage-system-demo/index.html"
+  },
+  {
+    "revision": "21693f4a05b360c96fd8",
+    "url": "/storage-system-demo/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "6216c0fd1f2b1473c1cf",
+    "url": "/storage-system-demo/static/js/2.e4dc7e7f.chunk.js"
+  },
+  {
+    "revision": "21693f4a05b360c96fd8",
+    "url": "/storage-system-demo/static/js/main.4d61f141.chunk.js"
+  },
+  {
+    "revision": "23d73729584ec649ac7e",
+    "url": "/storage-system-demo/static/js/runtime~main.4f5566f3.js"
+  }
+]);
